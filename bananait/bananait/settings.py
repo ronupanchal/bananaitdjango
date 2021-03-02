505cd495eaaf75fae13a3505c09cd1d89049f66d
Ronak Panchal <ronubca@gmail.com>
2021-03-02 22:48:03 +0530

    Changes in slider with carousel

--- bananait/bananait/settings.py
+++ bananait/bananait/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bananait.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join('templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
